fdeaaf223907f4ca35df6e5ee2d9db5f0fbcc298
sbwml <984419930@qq.com>
2021-09-07 13:50:01 +0800

    Increase the DKIM signin key length to 2048
    * Both DNS hosters dnspod and alibaba in mainland China have supported ultra-long TXT resolution values.
    * Increase the DKIM encryption length to 2048 to make the standard not obsolete.

--- install/start.sh
+++ install/start.sh
@@ -215,7 +215,7 @@ init(){
     
     mkdir -p /ewomail/dkim
     chown -R amavis:amavis /ewomail/dkim
-    amavisd genrsa /ewomail/dkim/mail.pem
+    amavisd genrsa /ewomail/dkim/mail.pem 2048
     chown -R amavis:amavis /ewomail/dkim
     
     service mysqld start
